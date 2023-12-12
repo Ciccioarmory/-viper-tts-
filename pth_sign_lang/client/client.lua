482e2144ad8bba908da2b6777fe3afdd82848b05
Ciccioarmory <ciccioarmory@hotmail.it>
2023-12-12 05:48:01 +0100

    added print

--- pth_sign_lang/client/client.lua
+++ pth_sign_lang/client/client.lua
@@ -1,18 +1,26 @@
-
 RegisterCommand(Config.command, function()
     SetNuiFocus(true, true)
-    SendNUIMessage({action = "show" })
+    SendNUIMessage({
+        action = "show"
+    })
 end)
 
 RegisterNUICallback("closeAll", function(data, cb)
     SetNuiFocus(false, false)
-    SendNUIMessage({action = "hide" })
-    cb{"ok"}
+    SendNUIMessage({
+        action = "hide"
+    })
+    cb {
+        "ok"
+    }
 end)
 
 RegisterNUICallback("ReadText", function(data, cb)
-    TriggerServerEvent("pth_sign_lang:playsound", getNearPlayers(Config.distance), GetEntityCoords(PlayerPedId()),data)
-    cb{"ok"}
+    print(json.encode(data))
+    TriggerServerEvent("pth_sign_lang:playsound", getNearPlayers(Config.distance), GetEntityCoords(PlayerPedId()), data)
+    cb {
+        "ok"
+    }
 end)
 
 function getNearPlayers(radius)
@@ -20,14 +28,14 @@ function getNearPlayers(radius)
     local ped = PlayerPedId()
     local coords = GetEntityCoords(ped)
     local playersList = GetActivePlayers()
-    for _,uPid in pairs(playersList) do
+    for _, uPid in pairs(playersList) do
         if NetworkIsPlayerConnected(uPid) then
             local uPed = GetPlayerPed(uPid)
-			local uCoords = GetEntityCoords(uPed)
-			local distance = #(coords - uCoords)
-			if distance <= radius then
-				table.insert(nearesPlayers,GetPlayerServerId(uPid))
-			end
+            local uCoords = GetEntityCoords(uPed)
+            local distance = #(coords - uCoords)
+            if distance <= radius then
+                table.insert(nearesPlayers, GetPlayerServerId(uPid))
+            end
         end
     end
     return nearesPlayers
