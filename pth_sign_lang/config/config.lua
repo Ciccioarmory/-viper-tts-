01df633fcd00f6358ddd6108299f0d2bdb477e70
Ciccioarmory <ciccioarmory@hotmail.it>
2023-12-12 06:01:08 +0100

    test

--- pth_sign_lang/config/config.lua
+++ pth_sign_lang/config/config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.command = "libras" --comando
-Config.lang = "pt-BR" -- language
+Config.command = "libras" -- comando
+Config.lang = "en-US" -- language
 Config.distance = 5 -- max distance sound
 Config.volume = 0.2 -- volume do audio
